0493cbcc2ec299de725dcd3861a98669a472b893
An-san8 <asanchez28@unomaha.edu>
2024-11-22 21:35:44 -0600

    Create test_television.py

---
+++ test_television.py
@@ -0,0 +1,20 @@
+import unittest
+from television import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_init(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
